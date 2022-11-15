f544399122e225873dcfc2e0c815dd66c53823fc
brixz <diazb6904@gmail.com>
2022-11-14 23:31:56 -0300

    termine el redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import Home from './Components/Home'
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Food</h1>
+      <Switch>
+        <Route exact path='/home' component={Home} />
+      </Switch>
     </div>
   );
 }
--- client/src/Redux/reduce.js
+++ client/src/Redux/reduce.js
@@ -1,18 +1,34 @@
 import {GET_ALL_RECIPES, GET_RECIPE_BY_NAME, GET_RECIPE_BY_ID, GET_ALL_DIETS, CREATE_RECIPE } from './actionstypes';
 
 const InitialState = {
-        recipes: [],
+        allrecipes: [],
+        recipe:[],
         diets: []  
 }
 
 const rootReducer =(state = InitialState,action)=>{
     switch (action.type) {
-        case value:
-            
-            break;
-    
-        default:
-            break;
+        case GET_ALL_RECIPES:    
+        return { ...state, 
+            allrecipes: action.payload
+        } 
+        case GET_RECIPE_BY_NAME:    
+        return { ...state, 
+            recipe: action.payload
+        }
+        case GET_RECIPE_BY_ID:    
+        return { ...state, 
+            recipe: action.payload
+        }
+        case GET_ALL_DIETS:    
+        return { ...state, 
+            diets: action.payload
+        }
+        case CREATE_RECIPE:    
+        return { ...state, 
+            recipe: action.payload
+        }
+    default: return {...state}    
     }
 }
 
